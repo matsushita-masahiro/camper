9bb48ee93c347e9353d3e48d5e549568904b6d13
fumionrails <fkforgit2323@gmail.com>
2019-01-10 08:21:18 +0000

    report

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
+  
   def index
+    if user_signed_in?
+      # イベントを取ってくる
+      @host_events = Event.where(user_id: current_user.id)
+      @events = Event.joins(:event_members).where("event_members.user_id=?", current_user.id)
+      # Relationshipモデルからfriendを取ってくる
+      @friends = Relationship.where(user_id: current_user.id).or(Relationship.where(friend_id: current_user.id))
+      # ルームを取得する
+      @rooms = Room.joins(:entries).where('entries.user_id = ?',current_user.id)
+    end
   end
 end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,3 @@
 class Relationship < ApplicationRecord
-  belongs_to :user
   has_many :users
 end
