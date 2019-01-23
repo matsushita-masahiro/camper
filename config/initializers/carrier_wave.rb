63cc42f6056bb83eb52c24ff5d0294f17f37aea3
fumionrails <fkforgit2323@gmail.com>
2019-01-23 07:14:10 +0000

    fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,16 +1,18 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 if Rails.env.production?
   CarrierWave.configure do |config|
-    
-    config.storage :fog
+    config.storage = :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory     =  ENV['S3_BUCKET']
-    
     config.fog_credentials = {
-      # Amazon S3用の設定
-      :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+        :provider              => 'AWS',
+        :aws_access_key_id     => ENV['S3_ACCESS_KEY_ID'],
+        :aws_secret_access_key => ENV['S3_SECRET_KEY_ID'],
+        :region                => ENV['S3_REGION']
     }
+    config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.fog_public = true
   end
 end
